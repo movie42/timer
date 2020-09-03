eb4c043144558594ab5d1d4f89484f203acf5f21
HYUNSU <movie3342@gmail.com>
2020-09-04 00:50:35 +0900

    try to get value array has error. need Fix it

--- src/index.js
+++ src/index.js
@@ -8,10 +8,28 @@ const month = document.getElementById("month");
 const day = document.getElementById("day");
 
 // 시간 입력 받아 출력하기
-const timer = (year, month, day) => {
-  const currentTime = new Date();
-  const selectTime = new Date(`${month} ${day}, ${year}`);
-  const restTime = selectTime - currentTime;
+
+const timer = (array) => {
+  if (array !== []) {
+    const input = [...array];
+    const currentTime = new Date();
+    const selectTime = new Date(
+      `${input[1] === undefined ? "12" : `${input[1]}`} ${
+        input[2] === undefined ? "25" : `${input[2]}`
+      }, ${input[0] === undefined ? "2020" : `${input[0]}`}`
+    );
+
+    const restTime = selectTime - currentTime;
+    const seconds = Math.floor((restTime / 1000) % 60);
+    const mins = Math.floor((restTime / (1000 * 60)) % 60);
+    const hours = Math.floor((restTime / (1000 * 60 * 60)) % 24);
+    const dayes = Math.floor(restTime / (1000 * 60 * 60 * 24));
+    outPutTime(dayes, hours, mins, seconds);
+  }
+};
+
+const outPutTime = (days, hours, mins, secs) => {
+  eventContainer.innerText = `${days}일 ${hours}시간 ${mins}분 ${secs}초 남았습니다.`;
 };
 
 // 입력란 공백으로 남겨놓고 Start나 엔터를 누를시에 경고메시지 표시하기
@@ -23,7 +41,8 @@ const handleSubmit = (event) => {
   const yearValue = year.value;
   const monthValue = month.value;
   const dayValue = day.value;
-  timer(yearValue, monthValue, dayValue);
+  const array = [yearValue, monthValue, dayValue];
+  timer(array);
 };
 
 const init = () => {
