903b2467414ca742f83f6fb5826f0b2cbcfe2461
HYUNSU <movie3342@gmail.com>
2020-09-04 10:15:11 +0900

    change input type, add start function in JS

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import "./styles.css";
 const eventOutput = document.querySelector(".eventOutput");
 const eventContainer = eventOutput.querySelector("h1");
 const form = document.querySelector("form");
-const year = document.getElementById("year");
-const month = document.getElementById("month");
-const day = document.getElementById("day");
+const dates = document.getElementById("dates");
 
 // 시간 입력 받아 출력하기
-let array = [];
+let data = [];
 
-const timer = () => {
+const start = () => {
+  timer(data);
+};
+
+const timer = (...date) => {
   const currentTime = new Date();
-  const selectTime = new Date(`${array[1]} ${array[2]}, ${array[0]}`);
+  const selectTime = new Date(date);
   const restTime = selectTime - currentTime;
   const seconds = Math.floor((restTime / 1000) % 60);
   const mins = Math.floor((restTime / (1000 * 60)) % 60);
@@ -23,7 +25,7 @@ const timer = () => {
 
 const outPutTime = (days, hours, mins, secs) => {
   eventContainer.innerText = `${days}일 ${hours}시간 ${mins}분 ${secs}초 남았습니다.`;
-  setInterval(timer, 1000);
+  setInterval(start, 1000);
 };
 
 // 입력란 공백으로 남겨놓고 Start나 엔터를 누를시에 경고메시지 표시하기
@@ -32,15 +34,14 @@ const outPutTime = (days, hours, mins, secs) => {
 
 const handleSubmit = (event) => {
   event.preventDefault();
-  const yearValue = year.value;
-  const monthValue = month.value;
-  const dayValue = day.value;
-  array.push(yearValue, monthValue, dayValue);
-  timer();
+  data = [];
+  const selectDates = dates.valueAsDate;
+  data.push(selectDates);
+  start();
 };
 
 const init = () => {
-  form.addEventListener("submit", handleSubmit, timer);
+  form.addEventListener("submit", handleSubmit);
 };
 
 init();
